9364c47cc6dbed961b7a99956ef2284ec775b786
jack <jackstowey@gmail.com>
2019-11-24 11:21:08 +0000

    Cleanup of boilerplate and adding of country table component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <country-table></country-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import CountryTable from './components/CountryTable.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    CountryTable,
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CountryTable.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Data here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountryTable',
+};
+</script>
+
+<style scoped>
+
+</style>
